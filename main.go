e0b85ac7acdaebb6f42935ca526412a4e8a322e5
i582 <i58215@voenmeh.ru>
2020-11-14 04:52:18 +0300

    cmd: moved RegisterStyle to main.go

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 	"os"
 
 	"github.com/i582/cfmt/internal/parser"
+	"github.com/i582/cfmt/internal/styles"
 )
+
+func RegisterStyle(name string, fn func(string) string) {
+	styles.Map[name] = func(f func(text string) string) func(text string) string {
+		return func(text string) string {
+			t := f(text)
+			t = fn(t)
+			return t
+		}
+	}
+}
 
 func Sprint(a ...interface{}) string {
 	text := fmt.Sprint(a...)
