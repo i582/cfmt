ae3b1d7a068bb89db20c10f042f490a3f7e4d4ff
i582 <i58215@voenmeh.ru>
2020-11-15 00:36:17 +0300

    internal: replaced functions for colors and styles with one template function

--- internal/styles.go
+++ internal/styles.go
@@ -5,49 +5,44 @@ import (
 	"github.com/muesli/termenv"
 )
 
-// Bold provides a bold style.
-func Bold(f func(text string) string) func(text string) string {
-	return func(text string) string {
-		t := f(text)
-		t = color.Bold.Sprint(t)
-		return t
-	}
-}
-
-// Italic provides a italic style.
-func Italic(f func(text string) string) func(text string) string {
-	return func(text string) string {
-		t := f(text)
-		t = color.OpItalic.Sprint(t)
-		return t
-	}
-}
-
-// CrossOut provides a crossout style.
-func CrossOut(f func(text string) string) func(text string) string {
-	return func(text string) string {
-		t := f(text)
-		t = color.OpStrikethrough.Sprint(t)
-		return t
-	}
-}
-
-// Underline provides a underline style.
-func Underline(f func(text string) string) func(text string) string {
-	return func(text string) string {
-		t := f(text)
-		t = color.OpUnderscore.Sprint(t)
-		return t
-	}
-}
-
-// Overline provides a overline style.
-func Overline(f func(text string) string) func(text string) string {
+func overline(f func(text string) string) func(text string) string {
 	return func(text string) string {
 		t := f(text)
 		out := termenv.String(t)
 		out = out.Overline()
 		t = out.String()
 		return t
+	}
+}
+
+func hexForegroundColor(cl string, f func(text string) string) func(text string) string {
+	return func(text string) string {
+		t := f(text)
+		out := termenv.String(t)
+		p := termenv.ColorProfile()
+		out = out.Foreground(p.Color(cl))
+		t = out.String()
+		return t
+	}
+}
+
+func hexBackgroundColor(cl string, f func(text string) string) func(text string) string {
+	return func(text string) string {
+		t := f(text)
+		out := termenv.String(t)
+		p := termenv.ColorProfile()
+		out = out.Background(p.Color(cl))
+		t = out.String()
+		return t
+	}
+}
+
+func colorTemplate(cl color.Color) func(f func(text string) string) func(text string) string {
+	return func(f func(text string) string) func(text string) string {
+		return func(text string) string {
+			t := f(text)
+			t = cl.Sprint(t)
+			return t
+		}
 	}
 }
